93a90c81711ff31fbd757bd726eeadc919943405
DANILO BELLASQUES <danilo.bellasques@senacsp.edu.br>
2025-02-17 17:03:27 -0300

    Resolvendo Pesquisa

---
+++ src/routes/index.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import * as usuarioController from '../controllers/usuarioController';
+
+const router = Router();
+
+// Rotas das etapas
+router.get('/etapa1', usuarioController.etapa1);
+router.post('/etapa1', usuarioController.etapa1Post);
+
+router.get('/etapa2', usuarioController.etapa2);
+router.post('/etapa2', usuarioController.etapa2Post);
+
+router.get('/etapa3', usuarioController.etapa3);
+router.post('/etapa3', usuarioController.etapa3Post);
+
+router.get('/etapa4', usuarioController.etapa4);
+router.post('/etapa4', usuarioController.etapa4Post);
+
+router.get('/etapa5', usuarioController.etapa5);
+router.post('/etapa5', usuarioController.etapa5Post);
+
+router.get('/etapa6', usuarioController.etapa6);
+router.post('/etapa6', usuarioController.etapa6Post);
+
+router.get('/etapa7', usuarioController.etapa7);
+
+// Rota para submissão final
+router.post('/submit', usuarioController.submit);
+
+// Rota de pesquisa
+router.get('/pesquisa', usuarioController.pesquisarUsuario);
+
+export default router;
